60e1a3dfb8629e695ed43529d0cbb2f88c0137ff
Deepak Maharana <dmaharana@gmail.com>
2017-12-30 20:02:29 +0530

    working code after chapter 2-12

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  firstMediaItem = {
+    id: 1,
+    name: 'Firebug',
+    medium: 'Series',
+    category: 'Science Fiction',
+    year: 2010,
+    watchedOn: 1294166565384,
+    isFavorite: false
+  };
 }
--- src/app/media-item/media-item.component.ts
+++ src/app/media-item/media-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-media-item',
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MediaItemComponent implements OnInit {
 
+  @Input() mediaItem;
   constructor() { }
 
   ngOnInit() {
