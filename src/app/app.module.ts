f13de516eac480f204643579db25035454534871
Deepak Maharana <dmaharana@gmail.com>
2017-12-30 23:59:36 +0530

    working code after chapter 4-02 angular forms with template driven approach

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
@@ -7,6 +8,7 @@ import { MediaItemComponent } from './media-item/media-item.component';
 import { MediaItemListComponent } from './media-item-list/media-item-list.component';
 import { FavoriteDirective } from './favorite.directive';
 import { CategoryListPipe } from './category-list.pipe';
+import { MediaItemFormComponent } from './media-item-form/media-item-form.component';
 
 
 @NgModule({
@@ -15,10 +17,12 @@ import { CategoryListPipe } from './category-list.pipe';
     MediaItemComponent,
     MediaItemListComponent,
     FavoriteDirective,
-    CategoryListPipe
+    CategoryListPipe,
+    MediaItemFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
