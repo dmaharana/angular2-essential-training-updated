6a32427c277b41a392029d917da71641befe290c
Deepak Maharana <dmaharana@gmail.com>
2017-12-31 08:54:56 +0530

    working code after chapter 5-05 service injection

--- src/app/media-item-form/media-item-form.component.ts
+++ src/app/media-item-form/media-item-form.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Validators, FormBuilder } from '@angular/forms';
+
+import { MediaItemService } from '../media-item.service';
 
 @Component({
   selector: 'app-media-item-form',
@@ -10,19 +12,21 @@ export class MediaItemFormComponent implements OnInit {
 
   form;
   onSubmit(mediaItem) {
-    console.log(mediaItem);
+    this.mediaItemService.add(mediaItem);
   }
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private mediaItemService: MediaItemService) { }
 
   ngOnInit() {
-    this.form = new FormGroup({
-      medium: new FormControl('Movies'),
-      name: new FormControl('', Validators.compose([
+    this.form = this.formBuilder.group({
+      medium: this.formBuilder.control('Movies'),
+      name: this.formBuilder.control('', Validators.compose([
         Validators.required,
         Validators.pattern('[\\w\\-\\s\\/]+')
       ])),
-      category: new FormControl(''),
-      year: new FormControl('', this.yearValidator),
+      category: this.formBuilder.control(''),
+      year: this.formBuilder.control('', this.yearValidator),
     }) ;
   }
 
